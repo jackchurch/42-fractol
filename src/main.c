81872c521f28d670c1641a2d06adde4c15ff2d55
Jack Church <jackchurch@Jacks-Mac-mini.local>
2022-10-14 10:29:49 +1030

    Beginning of mandelbrot

--- src/main.c
+++ src/main.c
@@ -27,12 +27,31 @@ int	main(void)
 
 	// void	*img;
 
+	int	width = 600;
+	int	height = 600;
 
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
-	img.img = mlx_new_image(vars.mlx, 20, 20);
+	vars.win = mlx_new_window(vars.mlx, width, height, "Hello world!");
+	img.img = mlx_new_image(vars.mlx, 50, 50);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+
+	// int x = 0;
+	// int y = 0;
+
+	// var a = 
+
+	// while (y < 55)
+	// {
+	// 	while (x < 55)
+	// 	{
+	// 		my_mlx_pixel_put(&img, x, y, 0x00FF0000);
+	// 		x++;
+	// 	}
+	// 	x = 5;
+	// 	y++;
+	// }
 	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+
 	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
 
 	mlx_key_hook(vars.win, key_hook, &vars);
